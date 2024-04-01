6d10e55eecd4917110128d5abfd4618fd8f6eaf0
Pedro Mantovani <pedro.mantovani@contabilivre.com>
2024-04-01 17:11:40 -0300

    fix : navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Link from "next/link"
 import {RxHamburgerMenu} from 'react-icons/rx'
 import {
@@ -8,45 +9,34 @@ import {
     SheetTitle,
     SheetTrigger,
   } from "@/components/ui/sheet"
-  
+import Image from "next/image";
+import Logo from '../../../public/logo.jpg'
+import {usePathname} from "next/navigation"
+import { cn } from "@/lib/utils"
+import { Navitem } from "./nav-item";
 const Navbar = () => {
     const NAV_ITEMS = [
         {
             label : "Quem Somos",
-            href : "#"
+            href : "/QuemSomos"
         },
         {
             label : "Home",
-            href : "#"
+            href : "/"
         },
-        // {
-        //     label : "HEALTHY LIVING",
-        //     href : "#"
-        // },
-        // {
-        //     label : "COMMUNITY",
-        //     href : "#"
-        // },{
-        //     label : "ABOUT",
-        //     href : "#"
-        // },
-        // ,{
-        //     label : "STORE",
-        //     href : "#"
-        // },
     ]
+    const pathName = usePathname()
+    const isActive = pathName == '/QuemSomos'
     return (  
         <nav className="color-white p-5 m-4">
             <div className="flex items-center justify-between md:justify-around lg:justify-around">
-                <Link href={'/'} className=" hover:text-[#E6533C] text-lg relative cursor-pointer">
-                    LOGO
+                <Link href={'/'} className=" hover:text-[#ffde59] text-lg relative cursor-pointer">
+                    <Image src={Logo} width={0} height={0} alt="logo" className="w-[50px] h-[50px] rounded-full"/>
                 </Link>
 
-                <div className="hidden md:flex lg:flex items-center gap-4 text-sm relative cursor-pointer ">
+                <div className="hidden md:flex lg:flex items-center gap-4 text-sm relative cursor-pointer" >
                     {NAV_ITEMS.map(item => (
-                        <Link href={`${item?.href}`} key={item?.label} className="hover:text-[#E6533C] text-xs ">
-                                {item?.label}
-                        </Link>
+                       <Navitem label={item.label} href={item.href}/>
                     ))}
                 </div>
 
@@ -54,15 +44,13 @@ const Navbar = () => {
                     <SheetTrigger className=" md:hidden">
                         <RxHamburgerMenu className="block h-6 w-6" aria-hidden="true" />
                     </SheetTrigger>
-                    <SheetContent className="">
+                    <SheetContent className="bg-[#ffde59] border-none">
                         <SheetHeader>
-                            <SheetTitle className="">Morada do sol</SheetTitle>
-                            <SheetDescription className="">
+                            <SheetTitle className="text-xl text-orange-500 font-bold">Morada do sol</SheetTitle>
+                            <SheetDescription className="text-xl">
                                 <div className="flex flex-col items-start mt-2 gap-4 text-sm relative cursor-pointer ">
                                     {NAV_ITEMS.map(item => (
-                                        <Link href={`${item?.href}`} key={item?.label} className="hover:text-[#E6533C] p-2">
-                                                {item?.label}
-                                        </Link>
+                                        <Navitem label={item.label} href={item.href}/>
                                     ))}
                                 </div>
                             </SheetDescription>
