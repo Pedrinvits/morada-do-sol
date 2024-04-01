ca44f454de56faaf4530bfe50dbd5bd862b81958
Pedro Mantovani <pedro.mantovani@contabilivre.com>
2024-04-01 17:15:51 -0300

    feat : add categories in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import '@/app/globals.css'
 import Navbar from "@/components/navbar";
+import Categories from "@/components/categories";
+import Products from "./products";
+
 
 export default function Home() {
   return (
-    <Navbar/>
+    <>
+      <Categories/>
+      <Products/>
+    </>
   );
 }
