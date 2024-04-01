f0acaa1aec9740a02526b4c6366124aecc791d24
Pedro Mantovani <pedro.mantovani@contabilivre.com>
2024-04-01 17:15:35 -0300

    feat : add navbar in all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
 import './globals.css'
 import { cn } from "@/lib/utils";
+import Navbar from "@/components/navbar";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -23,11 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning lang="pt-br">
       <body className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
-        )}>{children}</body>
+        )}><Navbar/>{children}</body>
     </html>
   );
 }
