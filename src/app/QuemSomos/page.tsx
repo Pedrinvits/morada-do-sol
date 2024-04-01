07322e6970110e474a85620146e97022128e1db4
Pedro Mantovani <pedro.mantovani@contabilivre.com>
2024-04-01 17:13:52 -0300

    feat : initial quem somos

---
+++ src/app/QuemSomos/page.tsx
@@ -0,0 +1,24 @@
+const QuemSomos = () => {
+    return ( 
+        <section className="container bg-[#ffde59] p-6 rounded-lg  sm:w-[80%] w-[80%] md:w-[80%]">
+            <h1 className="text-xl sm:text-3xl text-orange-500 font-bold mb-4">Nossa Empresa</h1>
+            <p className="text-sm sm:text-xl text-orange-400 font-bold text-justify">
+            Bem-vindo à experiência única e saborosa da nossa empresa <span className="text-green-700">Mel Silvestre Morada do Sol</span> !<br/>onde cada gota de mel é um testemunho da qualidade que cultivamos em nossa própria propriedade, em nossa empresa, nosso mel é mais do que um produto; é uma celebração da natureza em sua forma mais pura. 
+           </p>
+           <h1 className="text-xl sm:text-3xl text-orange-500 font-bold mb-4"> O que nos Torna Únicos</h1>
+           <p className="text-sm sm:text-xl text-orange-400 font-bold text-">
+           <span className="text-green-700">Produção Local e Sustentável</span> Nosso mel é colhido diretamente em nossa propriedade, onde garantimos a preservação do meio ambiente e a extração de um mel de qualidade.<br/>
+
+           <span className="text-green-700">Rigorosa Análise de Qualidade</span> Cada lote passa por análises rigorosas para garantir que apenas o mel mais puro e saboroso chegue à sua mesa. Transparência é a nossa promessa.<br/>
+
+            Pureza que Encanta Sem melaço e sem açúcar - apenas o autêntico néctar das flores silvestres, proporcionando uma experiência de sabor incomparável.
+
+            Deguste a Diversidade: Permita que seu paladar viaje por uma sinfonia de sabores, desde as delicadas notas florais até os robustos aromas da natureza intocada.
+
+            Explore a autenticidade, descubra a diferença. 
+           </p>
+        </section>
+     );
+}
+ 
+export default QuemSomos;
